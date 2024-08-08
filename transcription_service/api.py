84528fe630238092d481b08ced2473e46c746fa7
mmikolajczak <michal.mikolajczak@datarabbit.ai>
2024-08-09 00:13:31 +0200

    Add enqueuing jobs, and validating uploaded file type to the upload endpoint.

--- transcription_service/api.py
+++ transcription_service/api.py
@@ -1,37 +1,45 @@
 import shutil
-import uuid
 from datetime import datetime
 from pathlib import Path
-from typing import Dict, Optional
 
-import ffmpeg
-from fastapi import APIRouter, BackgroundTasks, File, HTTPException, Request, UploadFile
-from fastapi.responses import FileResponse, JSONResponse
-from pydantic import BaseModel
-from rq.job import Job
+from fastapi import APIRouter, File, HTTPException, Request, UploadFile
 
 from transcription_service import config
-from transcription_service.models import UploadResponse
+from transcription_service.models import MediaType, UploadResponse
+from transcription_service.transcription import determine_media_type, transcribe_audio, transcribe_video
 
 api_router = APIRouter()
 
 
 @api_router.post("/upload", response_model=UploadResponse)
-def upload(file: UploadFile = File(...)):
+def upload(request: Request, file: UploadFile = File(...)):
     # Important: can't be async.
     timestamp = datetime.now().astimezone().replace(microsecond=0).isoformat()
     filename = Path(file.filename).name
     reference_id = f"{timestamp}_{filename}"
 
-    # TODO: validate file format/type? (extension at least)
+    media_type = determine_media_type(config.UPLOADS_DIR / filename)
+    if media_type == MediaType.OTHER:
+        raise HTTPException(
+            status_code=415, detail="Unsupported media type. Only " "limited audio and video formats are supported."
+        )
+    # TODO: also possibly ad a check for the file size here / configurable max size.
+
     try:
-        with open(config.UPLOADS_DIR / filename, "wb") as f:
+        with open(config.UPLOADS_DIR / reference_id, "wb") as f:
             # We utilize shutil.copyfileobj from stdlib as it handles the chunking of larger files underneath.
             shutil.copyfileobj(file.file, f)
-            # TODO: redis enqueue
     except Exception as e:
-        raise HTTPException(status_code=500, detail="Error uploading the file")
+        raise HTTPException(status_code=500, detail="Error uploading the file.")
     finally:
         file.file.close()
+
+    if media_type == MediaType.VIDEO:
+        job_type = transcribe_video
+    elif media_type == MediaType.AUDIO:
+        job_type = transcribe_audio
+    # Else shouldn't ever happen as we handle the OTHER type earlier/before with different error code.
+
+    request.app.state.queue.enqueue(job_type, reference_id)
 
     return {"reference_id": reference_id}
