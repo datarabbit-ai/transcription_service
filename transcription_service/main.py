fe6406e7a7c81b531f978dec183fac99788d573a
mmikolajczak <michal.mikolajczak@datarabbit.ai>
2024-08-08 23:25:15 +0200

    Add validation of whether or not Redis connection was successfully established.

--- transcription_service/main.py
+++ transcription_service/main.py
@@ -3,10 +3,27 @@ from contextlib import asynccontextmanager
 import whisper
 from fastapi import FastAPI
 from redis import Redis
+from redis.exceptions import ConnectionError as RedisConnectionError
+from redis.exceptions import TimeoutError
 from rq import Queue
 
 from transcription_service import config
 from transcription_service.api import api_router
+
+
+def _validate_redis_connection(redis_conn: Redis) -> bool:
+    """
+    Validate the Redis connection.
+    :param redis_conn:
+    :return: whether the connection is was successful and can be used
+    """
+    try:
+        response = redis_conn.ping()
+        if not response:
+            return False
+    except (RedisConnectionError, TimeoutError) as e:
+        return False
+    return True
 
 
 @asynccontextmanager
@@ -20,6 +37,9 @@ async def lifespan(app: FastAPI):
 
     app.state.transcription_model = whisper.load_model(config.WHISPER_MODEL_NAME)
     app.state.redis_conn = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB)
+    redis_connection_successful = _validate_redis_connection(app.state.redis_conn)
+    if not redis_connection_successful:
+        raise ConnectionError("Startup failed. Could not connect to Redis.")
     app.state.queue = Queue(connection=app.state.redis_conn)
     yield
     """ 
