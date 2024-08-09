09e4aecdc00839c678bc84a0d6fc4dd6d1db4256
mmikolajczak <michal.mikolajczak@datarabbit.ai>
2024-08-10 00:11:59 +0200

    Loose the requirements for Whisper-related environment variables so that they have default values/do not force the API service to crash when they're not provided

--- transcription_service/config.py
+++ transcription_service/config.py
@@ -1,10 +1,13 @@
 import os
 from pathlib import Path
 
-WHISPER_MODEL_NAME = os.environ["WHISPER_MODEL_NAME"]
-WHISPER_MODEL_DEVICE = os.environ["WHISPER_MODEL_DEVICE"]
 REDIS_HOST = os.environ["REDIS_HOST"]
 REDIS_PORT = int(os.environ["REDIS_PORT"])
 REDIS_DB = int(os.environ["REDIS_DB"])
 UPLOADS_DIR = Path(os.environ["UPLOADS_DIR"])
 TRANSCRIPTIONS_DIR = Path(os.environ["TRANSCRIPTIONS_DIR"])
+
+# Default values for the whisper model are set as the codebase is shared by both API and worker, and the former
+# is independent/does not rely on them, so it shouldn't crash if they are not set.
+WHISPER_MODEL_NAME = os.environ.get("WHISPER_MODEL_NAME", "base")
+WHISPER_MODEL_DEVICE = os.environ.get("WHISPER_MODEL_DEVICE", "cpu")
